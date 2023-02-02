9cdeaf7ea551ec4eb8ea7bb9000bc9e5c5defd71
cassielawww <48407721+cassielawww@users.noreply.github.com>
2023-02-03 01:45:24 +0800

    Update script.js
    - Cleaned unnecessary codes
    - Clearer commenting

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,12 @@
-/* var openWeatherApi=  '582b12347eeccfd7da6831e8aac48bf6'; */
-
+// Create global variables
 var apiKey = '582b12347eeccfd7da6831e8aac48bf6';
 var apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=';
 var apiForecastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=';
 var apiMetrics = '&units=imperial';
 var cityArray = [];
-//var cityName = document.querySelector('#citySearch');
-//var url = apiURL + cityName + apiKey + apiMetrics;
 
-/*Search button*/
-
-
-function weatherAsk(){
-    document.getElementById("searchButton").innerHTML = alert("Hello World");
-    //searchBtn.addEventListener("click", fetchWeather());
-
-
-}
-
-//weatherAsk();
-//var button = select('#searchButton');
-
-
-
-//var searchBtn = $(".searchButton")
-
-//searchBtn.on("click", function(){
-//    cityName = $(this).siblings(".citySearch").val;
-//    console.log(cityName)
-//})
-
-//document.getElementById('searchButton');
-//searchBtn.addEventListener("click", fetchWeather());
-
-
+// Renders current weather results on html
 function currentWeather(weatherJSON){
-    //weatherJSON.preventDefault();
-    //console.log(weatherJSON);
-
     // Clear old results
     $('#weather-results').empty();
 
@@ -69,15 +38,11 @@ function currentWeather(weatherJSON){
     var renderWindspeed = document.createElement("p");
     renderWindspeed.textContent = "Wind Speed: " + weatherJSON.wind.speed + "  mph";
     currentResults.append(renderWindspeed);
-    
-
-    //search: function () {
-       // this.fetchWeather(document.querySelector(".inputValue").value);
 
 }
 
+// Render forecast results on html
 function forecastWeather(weatherJSON){
-    //console.log(weatherJSON);
     // Create a 5-day array for weather forecast
     var forecastArray = [];
     
@@ -92,8 +57,6 @@ function forecastWeather(weatherJSON){
     forecastHeader.textContent = "5-Day Forecast";
     forecastResults.append(forecastHeader);
     
-    //forecastResults.insertAdjacentHTML( 'beforeend', forecastHeader );
-
     // Popuplate the array with dates using a for loop
     for(i = 1; i < 6; i++) {
         var nextDate = dayjs().add(i, 'day').format("YYYY-MM-DD").toString() + " 00:00:00";
@@ -114,8 +77,6 @@ function forecastWeather(weatherJSON){
             var searchKey = dayjs(weatherJSON.list[a].dt_txt).format("YYYY-MM-DD HH:mm:ss").toString();
             if (searchKey.match(searchVal)){
                 console.log("match: "+ searchKey);
-                // Create a new div for each forecast day
-
                 //show forecast date
                 var forecastDate = document.createElement("h2");
                 forecastDate.textContent = dayjs(weatherJSON.list[a].dt_txt).format("YYYY-MM-DD");
@@ -158,20 +119,16 @@ $('#searchButton').on("click", function(event){
 })
 
 /*Click on search history then render weather results*/
-//$('#searchHistory').on("click", function(event){
 function clickHistory(cityName) {
-    // Get search string
-    //cityName = $(this).val();
-    //alert(cityName);
     // Query the weather API
     fetchWeather(cityName);
 }
 
-
+// Post/update search history on html page as buttons
 function searchResult(){
     $('#searchHistory').empty();
     var searchHistory = document.querySelector("#searchHistory");
-
+    
     for (i = 0;  i < cityArray.length; i++){
         var searchCity = document.createElement("button");
         var cityValue = cityArray[i];
@@ -180,20 +137,13 @@ function searchResult(){
         searchCity.setAttribute ('onclick', 'clickHistory(this.value)');
         searchCity.style = 'searchHistory';
         searchHistory.append(searchCity);
-        //document.getElementById('searchHistory').innerHTML = localStorage.getItem('cityArray');
-
-    }
-    
+    }   
 }
 
+// Query the API and post JSON for results rendering
 function fetchWeather(cityName){
-    
-    //var cityName = document.getElementById("searchButton");
-    //cityName.firstChild.nodeValue = query;
-
     var completeURL = apiURL + cityName + "&appid=" + apiKey + apiMetrics;
-
-        fetch(completeURL)
+    fetch(completeURL)
         .then((response) => response.json())
         .then((data) => currentWeather(data));
 
@@ -201,9 +151,5 @@ function fetchWeather(cityName){
     fetch(forecastURL)
         .then((response) => response.json())
         .then((data) => forecastWeather(data));
-
-    
+   
 }
-
-
-/*Search history*/
